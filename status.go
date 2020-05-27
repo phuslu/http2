eee4e8eea582c1f20621a22ad4312969b01ce643
phuslu <phus.lu@gmail.com>
2020-05-27 17:06:11 +0800

    cleanup 11

--- status.go
+++ status.go
@@ -29,33 +29,24 @@ package fasthttp2
 
 import (
 	"context"
-	"errors"
 	"fmt"
-
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes"
-	spb "google.golang.org/genproto/googleapis/rpc/status"
 )
 
 // Status represents an RPC status code, message, and details.  It is immutable
 // and should be created with New, Newf, or FromProto.
 type Status struct {
-	s *spb.Status
+	c Code
+	s string
 }
 
 // New returns a Status representing c and msg.
 func statusNew(c Code, msg string) *Status {
-	return &Status{s: &spb.Status{Code: int32(c), Message: msg}}
+	return &Status{c: c, s: msg}
 }
 
 // Newf returns New(c, fmt.Sprintf(format, a...)).
 func statusNewf(c Code, format string, a ...interface{}) *Status {
 	return statusNew(c, fmt.Sprintf(format, a...))
-}
-
-// FromProto returns a Status representing s.
-func statusFromProto(s *spb.Status) *Status {
-	return &Status{s: proto.Clone(s).(*spb.Status)}
 }
 
 // Err returns an error representing c and msg.  If c is OK, returns nil.
@@ -70,26 +61,18 @@ func statusErrorf(c Code, format string, a ...interface{}) error {
 
 // Code returns the status code contained in s.
 func (s *Status) Code() Code {
-	if s == nil || s.s == nil {
+	if s == nil || s.s == "" {
 		return codesOK
 	}
-	return Code(s.s.Code)
+	return Code(s.c)
 }
 
 // Message returns the message contained in s.
 func (s *Status) Message() string {
-	if s == nil || s.s == nil {
+	if s == nil || s.s == "" {
 		return ""
 	}
-	return s.s.Message
-}
-
-// Proto returns s's status as an spb.Status proto message.
-func (s *Status) Proto() *spb.Status {
-	if s == nil {
-		return nil
-	}
-	return proto.Clone(s.s).(*spb.Status)
+	return s.s
 }
 
 // Err returns an immutable error representing s; returns nil if s.Code() is OK.
@@ -97,59 +80,24 @@ func (s *Status) Err() error {
 	if s.Code() == codesOK {
 		return nil
 	}
-	return &statusError{e: s.Proto()}
-}
-
-// WithDetails returns a new status with the provided details messages appended to the status.
-// If any errors are encountered, it returns nil and the first error encountered.
-func (s *Status) WithDetails(details ...proto.Message) (*Status, error) {
-	if s.Code() == codesOK {
-		return nil, errors.New("no error details for status with code OK")
-	}
-	// s.Code() != OK implies that s.Proto() != nil.
-	p := s.Proto()
-	for _, detail := range details {
-		any, err := ptypes.MarshalAny(detail)
-		if err != nil {
-			return nil, err
-		}
-		p.Details = append(p.Details, any)
-	}
-	return &Status{s: p}, nil
-}
-
-// Details returns a slice of details messages attached to the status.
-// If a detail cannot be decoded, the error is returned in place of the detail.
-func (s *Status) Details() []interface{} {
-	if s == nil || s.s == nil {
-		return nil
-	}
-	details := make([]interface{}, 0, len(s.s.Details))
-	for _, any := range s.s.Details {
-		detail := &ptypes.DynamicAny{}
-		if err := ptypes.UnmarshalAny(any, detail); err != nil {
-			details = append(details, err)
-			continue
-		}
-		details = append(details, detail.Message)
-	}
-	return details
+	return &statusError{e: s.s}
 }
 
 // Error wraps a pointer of a status proto. It implements error and Status,
 // and a nil *Error should never be returned by this package.
 type statusError struct {
-	e *spb.Status
+	c Code
+	e string
 }
 
 func (e *statusError) Error() string {
-	return fmt.Sprintf("rpc error: code = %s desc = %s", Code(e.e.GetCode()), e.e.GetMessage())
+	return fmt.Sprintf("rpc error: code = %s desc = %s", Code(e.c), e.e)
 }
 
-// GRPCStatus returns the Status represented by se.
-func (e *statusError) GRPCStatus() *Status {
-	return statusFromProto(e.e)
-}
+// // GRPCStatus returns the Status represented by se.
+// func (e *statusError) GRPCStatus() *Status {
+// 	return statusFromProto(e.e)
+// }
 
 // Is implements future error.Is functionality.
 // A Error is equivalent if the code and message are identical.
@@ -158,7 +106,7 @@ func (e *statusError) Is(target error) bool {
 	if !ok {
 		return false
 	}
-	return proto.Equal(e.e, tse.e)
+	return e.e == tse.e
 }
 
 // FromError returns a Status representing err if it was produced from this
